cbce66e15df4ef5315802ae846fc8c6581442da9
maxiang <maxiang@haoshuo.com>
2022-05-26 11:22:00 +0800

    feat: add editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import type { Component } from "solid-js";
+import { editor } from "monaco-editor";
+import { createEffect, createSignal } from "solid-js";
 
 const App: Component = () => {
+  createEffect(() => {
+    editor.create(document.getElementById("container")!, {
+      value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join(
+        "\n"
+      ),
+      language: "javascript",
+      theme: "vs-dark",
+    });
+  }, []);
   return (
-    <div class="w-full h-full flex flex-col border-t">
+    <div class="w-full h-full flex flex-col border-t border-white dark:border-gray-700 ">
       <div class="flex flex-1">
-        <div class="w-12 h-full border-r"></div>
-        <div class="w-75 h-full border-r"></div>
-        <div class="flex-1"></div>
+        <div class="w-18 h-full bg-tertiary"></div>
+        <div class="w-75 h-full bg-secondary flex flex-col">
+          <div class="w-full h-12 shadow"></div>
+          <div class="flex-1"></div>
+        </div>
+        <div class="flex-1 bg-primary flex flex-col">
+          <div class="w-full h-12 shadow"></div>
+          <div class="flex-1" id="container"></div>
+        </div>
       </div>
-      <div class="w-full h-5 border-t"></div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
+        18: "4.5rem",
         75: "18.75rem",
+      },
+      backgroundColor: {
+        tertiary: "#202225",
+        secondary: "#2f3136",
+        primary: "#36393f",
       },
     },
   },
